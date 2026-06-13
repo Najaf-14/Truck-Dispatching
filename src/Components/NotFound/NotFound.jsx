314e47bec6d56c350b67ce8225af9aa2e6098a34
Najaf-14 <najafimran314@gmail.com>
2026-06-13 21:22:14 +0500

    Not Found File

---
+++ src/Components/NotFound/NotFound.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './NotFound.css';
+
+const NotFound = () => {
+  return (
+    <div className="notfound-container">
+      <div className="notfound-content">
+        <div className="notfound-code">404</div>
+        <h1>Page Not Found</h1>
+        <p>Oops! The page you're looking for doesn't exist or has been moved.</p>
+        <div className="notfound-buttons">
+          <Link to="/" className="home-btn">Go Back Home</Link>
+          <Link to="/contact" className="contact-btn">Contact Support</Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
