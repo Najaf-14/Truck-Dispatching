19060b41cbbd2871a57c27f285c14c68ffb028d5
Najaf-14 <najafimran314@gmail.com>
2026-06-13 16:20:10 +0500

    Created Pages

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { Routes ,Route } from 'react-router-dom'
+import Home from '../Pages/Home'
+import About from '../Pages/About'
+import Pricing from '../Pages/Pricing'
+import Services from '../Pages/Services'
+import Contact from '../Pages/Contact'
+import Driver from '../Pages/Driver'
+import NotFound from '../Pages/NotFound'
 
 function AppRoutes() {
   return (
-    <div>AppRoutes</div>
+    <Routes>
+        <Route path='/' element={<Home />} />
+         <Route path='/about' element={<About />} />
+        <Route path='/pricing' element={<Pricing />} />
+        <Route path='/services' element={<Services />} />
+        <Route path='/contact' element={<Contact />} /> 
+        <Route path='/driver' element={<Driver />} />
+        <Route path='*' element={<NotFound />} />
+    </Routes>
   )
 }
 
