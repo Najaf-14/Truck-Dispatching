7ca29db2a6dc19afe973a47dd08c36f88ef62a9d
Najaf-14 <najafimran314@gmail.com>
2026-06-14 23:41:12 +0500

    Added Component Auth

---
+++ src/Auth/LoginComp.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./LoginComp.css"
+
+function LoginComp() {
+  return (
+    <div>Login</div>
+  )
+}
+
+export default LoginComp;
---
+++ src/Auth/SignupComp.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./SignupComp.css"
+
+
+function SignupComp() {
+  return (
+    <div>Signup</div>
+  )
+}
+
+export default SignupComp;
