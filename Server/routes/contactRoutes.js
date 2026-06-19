b6e4d652df54871a3077de01e3a8ac7c9512548c
Najaf-14 <najafimran314@gmail.com>
2026-06-19 16:54:23 +0500

    contact message api created

--- Server/routes/contactRoutes.js
+++ Server/routes/contactRoutes.js
@@ -6,11 +6,18 @@ const {
   getAllMessages,
   deleteMessage,
 } = require("../App/controllers/contactController");
+const authMiddleware = require("../App/middleware/authMiddleware");
+const adminMiddleware = require("../App/middleware/adminMiddleware");
 
 router.post("/", createContact);
 
-router.get("/admin/message", getAllMessages);
+router.get("/admin/messages", getAllMessages);
 
-router.delete("/admin/message/:id", deleteMessage);
+router.delete(
+  "/admin/message/:id",
+  authMiddleware,
+  adminMiddleware,
+  deleteMessage,
+);
 
 module.exports = router;
