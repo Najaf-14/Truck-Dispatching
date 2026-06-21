b2ca6a8959ca57e9166c57abaec232bbc8bab1f7
Najaf-14 <najafimran314@gmail.com>
2026-06-21 15:05:18 +0500

    Contact Schema updated

--- Server/App/models/Contact.js
+++ Server/App/models/Contact.js
@@ -1,34 +1,34 @@
 const mongoose = require("mongoose");
 
 const contactSchema = mongoose.Schema(
-    {
-        fullName:{
-            type: String,
-            required: true,
-            trim: true,
-        },
-        email:{
-            type: String,
-            required: true,
-            trim: true,
-            unique: true,
-        },
-        
-        phoneNo:{
-            type: String,
-        },
-        subject:{
-            type: String,
-            required: true,
-        },
-        message:{
-            type: String,
-            required: true
-        },
+  {
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
     },
-    {
-        timestamps: true,
-    }
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+
+    phoneNo: {
+      type: String,
+    },
+    subject: {
+      type: String,
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
 );
 
 module.exports = mongoose.model("contact", contactSchema);
