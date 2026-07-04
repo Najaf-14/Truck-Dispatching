2310ce4f95d4ab9cad65ac1485727b850d1d4403
Najaf-14 <najafimran314@gmail.com>
2026-07-04 14:59:37 +0500

    Update and Delete apis

--- Server/routes/driverRoutes.js
+++ Server/routes/driverRoutes.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
-const { registerDriver, getAllDrivers } = require("../App/controllers/driverController.js");
+const {
+  registerDriver,
+  getAllDrivers,
+  editDriver,
+  deleteDriver,
+} = require("../App/controllers/driverController.js");
 
 router.post("/register", registerDriver);
 
-router.get("/all", getAllDrivers)
+router.get("/all", getAllDrivers);
+
+router.get("/update/:id", editDriver);
+
+router.get("/delete/:id", deleteDriver);
 
 module.exports = router;
